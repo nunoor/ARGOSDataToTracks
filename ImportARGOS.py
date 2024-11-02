eb796f8927cf3490bc5bb57239666fa26ccd8d0e
nunoor <nusratjnoor@gmail.com>
2024-11-02 19:20:47 -0400

    set input coordinate system

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -29,6 +29,9 @@ arcpy.AddField_management(outputFC,"TagID","LONG")
 arcpy.AddField_management(outputFC,"LC","TEXT")
 arcpy.AddField_management(outputFC,"Date","DATE")
 
+# Create the insert cursor
+cur = arcpy.da.InsertCursor(outputFC,['Shape@','TagID','LC','Date'])
+
 # Construct a while loop to iterate through all lines in the datafile
 # Open the ARGOS data file for reading
 inputFileObj = open(inputFile,'r')
@@ -83,12 +86,23 @@ while lineString:
             obsPoint.X = obsLon
             obsPoint.Y = obsLat
 
+            # Convert the point to a point geometry object with spatial reference
+            inputSR = arcpy.SpatialReference(4326)
+            obsPointGeom = arcpy.PointGeometry(obsPoint,inputSR)
+
+            # Create a feature object
+            feature = cur.insertRow((obsPointGeom,tagID,obsLC,obsDate.replace(".","/") + " " + obsTime))
+
         #Handle any error
         except Exception as e:
             print(f"Error adding record {tagID} to the output: {e}")
         
+    
     # Move to the next line so the while loop progresses
     lineString = inputFileObj.readline()
     
 #Close the file object
 inputFileObj.close()
+
+#Delete the cursor object
+del cur
